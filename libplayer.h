0a30628ad607669c952928d86b255f7f99b1389b
Yang Hongyang <burnef@gmail.com>
2009-05-13 21:39:34 +0800

    add gplv2 comments

--- libplayer.h
+++ libplayer.h
@@ -1,4 +1,18 @@
-//author:Yang Hongyang<burnef@gmail.com>
+/*  superlrc
+ *  Copyright (C) 2005-2007  Yang Hongyang<burnef@gmail.com>.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; under version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses>.
+ */
 
 #ifndef _LIBPLAYER_H
 #define _LIBPLAYER_H
