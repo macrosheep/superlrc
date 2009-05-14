e3c03d5871180e01f99931a9242bf9846d0b33fa
Yang Hongyang <burnef@gmail.com>
2009-05-14 20:17:32 +0800

    fix gpl copyright date...

--- downlrcdia.c
+++ downlrcdia.c
@@ -1,5 +1,5 @@
 /*  superlrc
- *  Copyright (C) 2005-2007  Yang Hongyang<burnef@gmail.com>.
+ *  Copyright (C) 2009-2011  Yang Hongyang<burnef@gmail.com>.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
